259f759e5d203aaa23384b4895e887e5499cf8bb
Akhilesh Kumar <akhilesh.n.kumar@accenture.com>
2019-02-20 16:16:44 +0530

    commitchanges

---
+++ sonardemo/src/main/java/accenture/business/Demo.java
@@ -0,0 +1,22 @@
+package accenture.business;
+
+//public class Demo {
+
+//	public static boolean check(String username,
+//			String password){
+//		if(username=="admin" && password=="admin"){
+//			return true;
+//		}
+//		else
+//			return false;
+//	}
+	
+	public class Demo {
+     
+		public static boolean check(String username,
+				int password){
+			 int var1 = 123;
+			return (username.equals("admin") && password == var1);
+	
+}
+}
